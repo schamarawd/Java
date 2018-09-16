0aadd2f9b99f3f4d954b81adc4d39cc9fa8c7241
Sandun Chamara <35859153+schamarawd@users.noreply.github.com>
2018-09-16 10:51:39 +0530

    Updated Applet

--- MyApplet.java
+++ MyApplet.java
@@ -4,54 +4,68 @@ public class MyApplet extends Applet{
 	public void paint(Graphics g){
 		g.setColor(Color.red);
 		g.drawString("Assignment for Java",05,10);
-		g.drawString("Created by Sandun Chamara",05,240);
+		g.drawString("Created by Sandun Chamara",05,290);
 				
-		for(int y=220; y>19;y--){
+		for(int y=270; y>19;y--){
 			g.setColor(Color.blue);
-			g.drawLine(10,y,80,y);
-			g.setColor(Color.yellow);
-			g.drawLine(80,y,150,y);
-			g.setColor(Color.red);
-			g.drawLine(150,y,220,y);
-			g.setColor(Color.white);
-			g.drawLine(220,y,290,y);
-			g.setColor(Color.orange);
-			g.drawLine(290,y,360,y);
-			try{Thread.sleep(50);}
+			g.drawLine(10,y,80,y);	
+			try{Thread.sleep(10);}
 			catch(Exception e){};
 		}
-
-			for(int a=220; a>179;a--){
-			g.setColor(Color.orange);
-			g.drawLine(360,a,430,a);
-			try{Thread.sleep(50);}
-			catch(Exception e){};
-			}		
-			for(int a=180; a>139;a--){
-			g.setColor(Color.white);
-			g.drawLine(360,a,430,a);
-			try{Thread.sleep(50);}
-			catch(Exception e){};
-			}		
-			for(int a=140; a>99;a--){
-			g.setColor(Color.red);
-			g.drawLine(360,a,430,a);
-			try{Thread.sleep(50);}
-			catch(Exception e){};
-			}
-			for(int a=100; a>59;a--){
+		for(int y=270; y>19;y--){
 			g.setColor(Color.yellow);
-			g.drawLine(360,a,430,a);
-			try{Thread.sleep(50);}
+			g.drawLine(81,y,150,y);
+			try{Thread.sleep(10);}
+			catch(Exception e){};
+		}
+		for(int y=270; y>19;y--){
+			g.setColor(Color.red);
+			g.drawLine(151,y,220,y);
+			try{Thread.sleep(10);}
+			catch(Exception e){};
+		}
+		for(int y=270; y>19;y--){
+			g.setColor(Color.white);
+			g.drawLine(221,y,290,y);
+			try{Thread.sleep(10);}
+			catch(Exception e){};
+		}
+		for(int y=270; y>19;y--){
+			g.setColor(Color.orange);
+			g.drawLine(291,y,360,y);
+			try{Thread.sleep(10);}
+			catch(Exception e){};
+		}
+		//sub
+			for(int a=270; a>219;a--){
+			g.setColor(Color.orange);
+			g.drawLine(361,a,430,a);
+			try{Thread.sleep(10);}
+			catch(Exception e){};
+			}		
+			for(int a=220; a>169;a--){
+			g.setColor(Color.white);
+			g.drawLine(361,a,430,a);
+			try{Thread.sleep(10);}
+			catch(Exception e){};
+			}		
+			for(int a=170; a>119;a--){
+			g.setColor(Color.red);
+			g.drawLine(361,a,430,a);
+			try{Thread.sleep(10);}
 			catch(Exception e){};
 			}
-			for(int a=60; a>19;a--){
+			for(int a=120; a>69;a--){
+			g.setColor(Color.yellow);
+			g.drawLine(361,a,430,a);
+			try{Thread.sleep(10);}
+			catch(Exception e){};
+			}
+			for(int a=70; a>19;a--){
 			g.setColor(Color.blue);
-			g.drawLine(360,a,430,a);
-			try{Thread.sleep(50);}
+			g.drawLine(361,a,430,a);
+			try{Thread.sleep(10);}
 			catch(Exception e){};
 			}
-			
-	
 	}
 }
